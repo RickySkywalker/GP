8a93c9d1a77e794508620a1d469030be1f33ba94
prulev <zwangfr@connect.ust.hk>
2021-11-23 22:42:35 +0800

    aaa

--- Group Assignment/event.h
+++ Group Assignment/event.h
@@ -17,8 +17,6 @@ private:
     int change_VP_by;
     int change_DEFCON;
 
-    int aaa;
-
 public:
     EVENT(std::string event_name, std::string event_description, Superpower belong_to, int resource_point, int VP, int DEFCON);
 
